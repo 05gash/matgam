7a3cb8dfeb67b1836ffa80ca8e7f728ecd6e4ae1
GeorgeProsser <georgeprosser2@gmail.com>
2017-03-17 13:00:58 +0000

    cleaner stuff

--- matgam.js
+++ matgam.js
@@ -1,13 +1,22 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'matgam', { preload: preload, create: create, update: update, render: render });
+window.onload = function() {
 
-function preload() {
-}
+    //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
+    //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
+    //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
 
-function create() {
-}
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
 
-function update() {
-}
+    function preload () {
 
-function render() {
-}
+        game.load.image('logo', 'phaser.png');
+
+    }
+
+    function create () {
+
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+
+    }
+
+};
