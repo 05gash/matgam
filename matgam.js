cd8d1586f3f7f152286a23f01fcec167b5a6665c
GeorgeProsser <georgeprosser2@gmail.com>
2017-03-17 12:39:08 +0000

    Added directory structure

---
+++ matgam.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'matgam', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+}
+
+function create() {
+}
+
+function update() {
+}
+
+function render() {
+}
