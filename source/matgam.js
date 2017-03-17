0f12ea4e6c0b3692104ba4b125c6d5e95c41b38c
GeorgeProsser <georgeprosser2@gmail.com>
2017-03-17 13:13:42 +0000

    things

--- source/matgam.js
+++ source/matgam.js
@@ -3,7 +3,7 @@ window.onload = function() {
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'matgam', { preload: preload, create: create, update: update });
 
   function preload() {
-    game.load.image('logo', 'phaser.png');
+    
   }
 
   function create() {
