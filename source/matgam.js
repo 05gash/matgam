49243d18493c2c01409719a3e61ee3b93e94d4ee
G. Ash <ga354@pccl022.pwf.cl.cam.ac.uk>
2017-03-17 14:59:44 +0000

    wat

--- source/matgam.js
+++ source/matgam.js
@@ -1,22 +1,31 @@
-window.onload = function() {
 
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'matgam', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, render: render });
 
-  function preload() {
-    game.load.image('wood', 'assets/sprites/wood.png')
-    
-    var materialButtons = [];
-    materialButtons += new MaterialButton('Wood', 'wood');
-    
-    game.stage.backgroundColor = '#cccccc';
-  }
+function preload() {
 
-  function create() {
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
-  }
+    game.load.image('atari1', '/assets/sprites/wood.png');
+    game.load.image('atari2', '/assets/sprites/aluminium.png');
+    game.load.image('atari4', '/assets/sprites/iron.png');
 
-  function update() {
-  }
+}
 
-};
+function create() {
+
+    //  This returns an array of all the image keys in the cache
+    var images = game.cache.getKeys(Phaser.Cache.IMAGE);
+
+    var test = game.add.group();
+
+    //  Now let's create some random sprites and enable them all for drag and 'bring to top'
+    for (var i = 0; i < 20; i++)
+    {
+        var tempSprite = test.create(game.world.randomX, game.world.randomY, game.rnd.pick(images));
+        tempSprite.inputEnabled = true;
+        tempSprite.input.enableDrag(false, true);
+    }
+
+}
+
+function render() {
+    game.debug.inputInfo(32, 32);
+}
